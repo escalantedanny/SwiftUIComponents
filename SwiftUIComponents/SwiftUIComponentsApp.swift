6a38172254b8f48a96fcaf2efdac2918108bedd6
Danny Escalante <danny.escalante@gmail.com>
2025-02-05 16:41:44 -0300

    first commit

---
+++ SwiftUIComponents/SwiftUIComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIComponentsApp.swift
+//  SwiftUIComponents
+//
+//  Created by Danny Escalante  on 05-02-25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
