5478791f5adc5b20ac1e3e487d9894bb88edd875
Jona <jonathan.munoz@klap.cl>
2025-02-05 22:22:36 -0300

    is add new navigation menu

---
+++ SwiftUIComponents/Sources/Utils/MenuItemDetail.swift
@@ -0,0 +1,29 @@
+//
+//  MenuItemDetail.swift
+//  SwiftUiComponents
+//
+//  Created by Jonathan Muñoz on 25-12-24.
+//
+
+import SwiftUI
+
+struct MenuItemDetail: View {
+    let item: MenuItems
+    
+    
+    var body: some View {
+        switch item.id {
+        case 1:
+            Text("No Item available yet")
+            
+        case 2 :
+            Text("No Item available yet")
+        default:
+            Text("No Item available yet")
+        }
+    }
+}
+
+#Preview {
+    MenuItemDetail(item: menuItem[0])
+}
